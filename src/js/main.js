495c3eec5d8159e2988261ebe84091f8e8a36fe9
claradelvecchio <clara.anquetil@studentfr.ch>
2025-02-03 12:18:39 +0100

    fini :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,36 @@
 var button = document.querySelector(".button");
 var numbers = document.querySelector(".number");
+var number = 147;
+
 function changeColor() {
   button.classList.toggle("is-active");
-  numbers.classList.toggle("is-active");
-}
-button.addEventListener("click", changeColor);
 
-// A finir (metre les animation js et nombre qui augmentent )
+  if (button.classList.contains("is-active")) {
+    button.style.backgroundColor = "red";
+    numbers.style.color = "white";
+  } else {
+    button.style.backgroundColor = "white";
+    numbers.style.color = "black";
+  }
+
+  setTimeout(resetButton, 300);
+}
+
+function incrementNumber() {
+  if (number < 200) {
+    number++;
+    numbers.textContent = number;
+  }
+}
+
+function resetButton() {
+  button.classList.remove("is-active");
+  button.style.backgroundColor = "white";
+  numbers.style.color = "black";
+}
+setTimeout(() => {
+  numbers.style.color = "black";
+}, 10);
+
+button.addEventListener("click", changeColor);
+button.addEventListener("click", incrementNumber);
